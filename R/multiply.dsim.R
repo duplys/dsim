b819e6f80cb76607353cfd2e686601e82a6ad563
Paul Duplys <paul.duplys@gmail.com>
2013-02-20 21:46:40 +0100

    Adding an R package skeleton
    Adding skeleton files required for an R package, scaffolds for the
    actual DNA simulator functions.

---
+++ R/multiply.dsim.R
@@ -0,0 +1,18 @@
+# This file is part of dsim R package.
+#
+# dsim R package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# dsim R package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with dsim R package. If not, see <http://www.gnu.org/licenses/>.
+
+multiply.dsim <- function(x, ...) {
+	cat("this is dsim multiply!\n")
+}
